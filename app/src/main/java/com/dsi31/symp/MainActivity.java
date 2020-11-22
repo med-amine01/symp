ae784cd91e03b2303a5e26f7d6f11396e8560116
med_amine01 <camine500@gmail.com>
2020-11-22 20:18:28 +0100

    Initial commit

---
+++ app/src/main/java/com/dsi31/symp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dsi31.symp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
